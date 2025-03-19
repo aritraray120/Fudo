2fd55d6b4a538ae9371e618d9870712bc076eb8b
Aritra Ray <aritraray120@gmail.com>
2025-03-19 17:21:28 +0530

    Update

--- frontend/src/components/Download.jsx
+++ frontend/src/components/Download.jsx
@@ -1,22 +1,17 @@
+
+
 import { assets } from "../assets/assets";
 
 function Download() {
   return (
-    <div className="text-center text-lg font-medium mb-[185px] px-4 " id="download">
-      <p className="text-3xl sm:text-[45px] leading-[40px] sm:leading-[60px] font-[700] text-[#333333] mb-10">
-        For Better Experience Download <br className="hidden sm:block" /> Tomato App
-      </p>
-      <div className="flex justify-center gap-4 mt-6 flex-wrap">
-        <img
-          src={assets.play_store}
-          className="w-[160px] sm:w-[210px] hover:scale-105 transition"
-          alt="Play Store"
-        />
-        <img
-          src={assets.app_store}
-          className="w-[160px] sm:w-[210px] hover:scale-105 transition"
-          alt="App Store"
-        />
+    <div className="flex flex-col items-center text-center gap-10 mb-40">
+      <h2 className="text-3xl sm:text-5xl font-bold text-[#333333]">
+        For a Smoother Experience,<br />Try Fudo App!
+      </h2>
+      <div className="flex gap-5">
+        {[assets.play_store, assets.app_store].map((src, i) => (
+          <img key={i} src={src} alt="" className="w-40 sm:w-48 transition-transform hover:scale-110" />
+        ))}
       </div>
     </div>
   );
